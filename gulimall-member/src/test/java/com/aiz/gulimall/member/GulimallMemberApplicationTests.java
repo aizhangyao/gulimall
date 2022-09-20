0d599349d13f76011c21bc04b7881b45651e40f7
ZhangYao <zhangyao_xz@foxmail.com>
2022-09-20 13:35:45 +0800

    init project

---
+++ gulimall-member/src/test/java/com/aiz/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiz.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
