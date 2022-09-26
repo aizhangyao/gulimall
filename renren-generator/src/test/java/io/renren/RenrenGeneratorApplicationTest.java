4333c5a361c5354463db818f83b33b2ba123e2be
ZhangYao <zhangyao_xz@foxmail.com>
2022-09-26 18:09:59 +0800

    add renren-generator module

---
+++ renren-generator/src/test/java/io/renren/RenrenGeneratorApplicationTest.java
@@ -0,0 +1,17 @@
+package io.renren;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @ClassName RenrenApplicationTest
+ * @Description 测试类
+ * @Author ZhangYao
+ * @Date Create in 16:07 2022/9/20
+ * @Version 1.0
+ */
+class RenrenGeneratorApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
