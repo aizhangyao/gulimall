bf396a0d7c4a4e20ae631bbbc2e7bce42ef65da3
ZhangYao <zhangyao_xz@foxmail.com>
2022-09-30 11:07:06 +0800

    add exception handler and JSR303

--- gulimall-product/src/main/java/com/aiz/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/aiz/gulimall/product/entity/BrandEntity.java
@@ -1,12 +1,23 @@
 package com.aiz.gulimall.product.entity;
 
+import com.aiz.common.valid.AddGroup;
+import com.aiz.common.valid.ListValue;
+import com.aiz.common.valid.UpdateGroup;
+import com.aiz.common.valid.UpdateStatusGroup;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
 /**
  * 品牌
  * 
@@ -22,15 +33,20 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌id
 	 */
+	@NotNull(message = "修改必须指定品牌id",groups = {UpdateGroup.class})
+	@Null(message = "新增不能指定id",groups = {AddGroup.class})
 	@TableId
 	private Long brandId;
 	/**
 	 * 品牌名
 	 */
+	@NotBlank(message = "品牌名必须提交",groups = {AddGroup.class,UpdateGroup.class})
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@NotBlank(groups = {AddGroup.class})
+	@URL(message = "logo必须是一个合法的url地址",groups={AddGroup.class,UpdateGroup.class})
 	private String logo;
 	/**
 	 * 介绍
@@ -39,14 +55,21 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 显示状态[0-不显示；1-显示]
 	 */
+	//@Pattern()
+	@NotNull(groups = {AddGroup.class, UpdateStatusGroup.class})
+	@ListValue(vals={0,1},groups = {AddGroup.class, UpdateStatusGroup.class})
 	private Integer showStatus;
 	/**
 	 * 检索首字母
 	 */
+	@NotEmpty(groups={AddGroup.class})
+	@Pattern(regexp="^[a-zA-Z]$",message = "检索首字母必须是一个字母",groups={AddGroup.class,UpdateGroup.class})
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@NotNull(groups={AddGroup.class})
+	@Min(value = 0,message = "排序必须大于等于0",groups={AddGroup.class,UpdateGroup.class})
 	private Integer sort;
 
 }
