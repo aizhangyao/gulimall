fc7bc01dbc539206677453f5a61a4396c120108b
ZhangYao <zhangyao_xz@foxmail.com>
2022-09-30 10:42:56 +0800

    整合OSS & 添加第三方服务gulimall-third-party

---
+++ gulimall-third-party/src/test/java/com/aiz/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,30 @@
+package com.aiz.gulimall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+
+    @Autowired
+    private OSSClient ossClient;
+
+    @Test
+    public void testUpdateUseSpringCloud() throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream("/Users/zhangyao/Picture/square/zhangyao-logo.png");
+        ossClient.putObject("gulimall-aiz", "zhangyao-logo.png",inputStream);
+        ossClient.shutdown();
+        System.out.println("上传成功....");
+    }
+
+    @Test
+    void contextLoads() {
+
+    }
+}
