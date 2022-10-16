ce631190d5306046e84ffe9216a8a6138b4657ec
ZhangYao <zhangyao_xz@foxmail.com>
2022-10-16 19:31:45 +0800

    feat(member):新增查询会员地址接口

--- gulimall-member/src/main/java/com/aiz/gulimall/member/controller/MemberReceiveAddressController.java
+++ gulimall-member/src/main/java/com/aiz/gulimall/member/controller/MemberReceiveAddressController.java
@@ -1,10 +1,12 @@
 package com.aiz.gulimall.member.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,15 @@ import com.aiz.common.utils.R;
 public class MemberReceiveAddressController {
     @Autowired
     private MemberReceiveAddressService memberReceiveAddressService;
+
+    /**
+     * 根据会员id查询会员的所有地址
+     */
+    @GetMapping(value = "/{memberId}/address")
+    public List<MemberReceiveAddressEntity> getAddress(@PathVariable("memberId") Long memberId) {
+        List<MemberReceiveAddressEntity> addressList = memberReceiveAddressService.getAddress(memberId);
+        return addressList;
+    }
 
     /**
      * 列表
