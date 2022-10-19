c1fc8e6b237383d7026c8453ef36ed2c890cdf40
ZhangYao <zhangyao_xz@foxmail.com>
2022-10-19 17:45:16 +0800

    feat(order): spring本地事务失效问题

--- gulimall-order/src/main/java/com/aiz/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/aiz/gulimall/order/GulimallOrderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
@@ -35,8 +36,20 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  *  (五)、监听消息：使用@RabbitListener；必须有@EnableRabbit
  *      @RabbitListener：类+方法上
  *      @RabbitHandler：标在方法上
+ *
+ *  本地事务失效问题
+ *  同一个对象内方法互相调用默认失败，原因 绕过了代理对象，事务使用代理对象来控制的。
+ *  解决：使用代理对象来调用事务方法
+ *  1)、引入aop-stater; spring-boot-starter-aop; 引入了aspectj
+ *  2)、@EnableAspectJAutoProxy(exposeProxy = true)开启aspectj动态代理功能。以后所有的动态代理都是aspectj创建的(即使没有接口也可以创建动态代理)
+ *      对外暴露代理对象
+ *  3)、本类互相调用调用对象
+ *         OrderServiceImpl orderService = (OrderServiceImpl) AopContext.currentProxy();
+ *         orderService.b();
+ *         orderService.c();
  */
 
+@EnableAspectJAutoProxy(exposeProxy = true)
 @EnableRedisHttpSession
 @EnableRabbit
 @MapperScan("com.aiz.gulimall.order.dao")
--- gulimall-order/src/main/java/com/aiz/gulimall/order/service/impl/OrderServiceImpl.java
+++ gulimall-order/src/main/java/com/aiz/gulimall/order/service/impl/OrderServiceImpl.java
@@ -24,6 +24,7 @@ import com.aiz.gulimall.order.vo.WareSkuLockVo;
 import com.alibaba.fastjson.TypeReference;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -51,6 +52,7 @@ import com.aiz.common.utils.Query;
 import com.aiz.gulimall.order.dao.OrderDao;
 import com.aiz.gulimall.order.entity.OrderEntity;
 import com.aiz.gulimall.order.service.OrderService;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestAttributes;
@@ -156,6 +158,30 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
         CompletableFuture.allOf(addressFuture, cartInfoFuture).get();
 
         return confirmVo;
+    }
+
+    // 同一个对象内 事务方法互调默认失效，原因：绕过了代理对象。
+    // 事务使用代理对象来控制的
+    @Transactional(timeout = 30) //a事务的所有设置就传播到了和她公用一个事务的方法
+    public void a() {
+//        // b,c做任何设置都没用，都是和a公用一个事务
+//        this.b();//没用
+//        this.c();//没用
+        OrderServiceImpl orderService = (OrderServiceImpl) AopContext.currentProxy();
+        orderService.b();
+        orderService.c();
+
+//        bService.b();//a事务
+//        cService.c();//新事物(不回滚)
+        int i = 10 / 0;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, timeout = 20)
+    public void b() {
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 20)
+    public void c() {
     }
 
     /**
