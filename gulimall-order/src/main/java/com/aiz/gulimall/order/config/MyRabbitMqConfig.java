ec70a85a7e149ea04540ff12e1f14693240c5cca
ZhangYao <zhangyao_xz@foxmail.com>
2022-10-21 16:48:23 +0800

    feat(order): 自动创建Queue、Exchange、Binding

---
+++ gulimall-order/src/main/java/com/aiz/gulimall/order/config/MyRabbitMqConfig.java
@@ -0,0 +1,90 @@
+package com.aiz.gulimall.order.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+/**
+ * @ClassName MyRabbitMqConfig
+ * @Description 容器中的Queue、Exchange、Binding都会自动创建(RabbitMQ不存在的情况); RabbitMQ只要有，@Bean声明属性变化也不会覆盖
+ * @Author ZhangYao
+ * @Date Create in 16:04 2022/10/21
+ * @Version 1.0
+ */
+@Configuration
+public class MyRabbitMqConfig {
+
+    /**
+     * 死信队列
+     */
+    @Bean
+    public Queue orderDelayQueue() {
+        /*
+            Queue(String name,  队列名字
+            boolean durable,  是否持久化
+            boolean exclusive,  是否排他
+            boolean autoDelete, 是否自动删除
+            Map<String, Object> arguments) 属性
+         */
+        HashMap<String, Object> arguments = new HashMap<>();
+        arguments.put("x-dead-letter-exchange", "order-event-exchange");
+        arguments.put("x-dead-letter-routing-key", "order.release.order");
+        // 消息过期时间 1分钟
+        arguments.put("x-message-ttl", 60000);
+        Queue queue = new Queue("order.delay.queue", true, false, false, arguments);
+        return queue;
+    }
+
+    /**
+     * 普通队列
+     */
+    @Bean
+    public Queue orderReleaseQueue() {
+        Queue queue = new Queue("order.release.order.queue", true, false, false);
+        return queue;
+    }
+
+    /**
+     * TopicExchange
+     */
+    @Bean
+    public Exchange orderEventExchange() {
+        /*
+         *   String name,
+         *   boolean durable,
+         *   boolean autoDelete,
+         *   Map<String, Object> arguments
+         * */
+        return new TopicExchange("order-event-exchange", true, false);
+    }
+
+    @Bean
+    public Binding orderCreateBinding() {
+        /*
+         * String destination, 目的地（队列名或者交换机名字）
+         * DestinationType destinationType, 目的地类型（Queue、Exhcange）
+         * String exchange,
+         * String routingKey,
+         * Map<String, Object> arguments
+         * */
+        return new Binding("order.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.create.order",
+                null);
+    }
+
+    @Bean
+    public Binding orderReleaseBinding() {
+        return new Binding("order.release.order.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.order",
+                null);
+    }
+}
