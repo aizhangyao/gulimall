7615401ac6bf3cf0c22be5b8212fbc7e3c28d588
ZhangYao <zhangyao_xz@foxmail.com>
2022-10-27 18:50:35 +0800

    feat(gulimall): 全服务配置 流控响应异常处理

---
+++ gulimall-order/src/main/java/com/aiz/gulimall/order/config/GulimallOrderSentinelConfig.java
@@ -0,0 +1,23 @@
+package com.aiz.gulimall.order.config;
+
+import com.aiz.common.exception.BizCodeEnum;
+import com.aiz.common.utils.R;
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSON;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class GulimallOrderSentinelConfig implements BlockExceptionHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws Exception {
+        R error = R.error(BizCodeEnum.TO_MANY_REQUEST.getCode(), BizCodeEnum.TO_MANY_REQUEST.getMsg());
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.getWriter().write(JSON.toJSONString(error));
+    }
+}
